56a3340b4d629625e02596d1fc7ec256ed3e9a70
justinmrks <justinmarks19@gmail.com>
2020-09-23 19:34:19 -0500

    added basic components

---
+++ src/components/pages/Footer.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            <div className='footer'>
+    <div>
+      <h3>Features</h3>
+    
+      <div>
+        Link Shortening
+        Branded Links
+        Analytics
+      </div>
+    </div>
+  
+    <div>
+      <h3>Resources</h3>
+    
+      <div>
+        Blog
+        Developers
+        Support
+      </div>
+    </div>
+  
+   <div>
+      <h3>Company</h3>
+    
+      <div>
+        About
+        Our Team
+        Careers
+        Contact
+      </div>
+   </div>
+</div>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/components/pages/Short.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Short = () => {
+    return (
+        <div>
+            <div>
+  <input type="text" />  
+  <button>Shorten It!</button>
+        </div>
+        <div>
+    <h2>Advanced Statistics</h2>
+  
+    <p>
+      Track how your links are performing across the web with our 
+      advanced statistics dashboard.
+    </p>
+</div>
+        </div>
+    )
+}
+
+export default Short
